6bf625255feac0774358ad7c3453da07d13e4683
Nick <nick@cracchiolo.com>
2016-05-11 12:51:09 -0700

    Added to NCSwitch. Just needs assets made and added to textures

--- FiveByFive/NCSwitch.swift
+++ FiveByFive/NCSwitch.swift
@@ -9,11 +9,15 @@
 import SpriteKit
 
 class NCSwitch:SKSpriteNode {
+    //A tile that acts as Switch. Down is on, Up is off
     var switchOn:Bool = false
+    let key:String
     
-    init() {
+    init(withName:String,keyForDefaultsItem:String) {
         let texture = SKTexture(imageNamed: "switch")
+        self.key = keyForDefaultsItem
         super.init(texture: texture, color: UIColor.whiteColor(), size: texture.size())
+        self.name = withName
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -21,13 +25,31 @@ class NCSwitch:SKSpriteNode {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         animate()
     }
+    func isOn() -> Bool {
+        return switchOn
+    }
+    func updateSwitchValue() -> Bool {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(valueForSwitch(), forKey: key)
+        return defaults.synchronize() //Possibly change to happen when leaving the scene
+    }
+    func valueForSwitch() -> Int {
+        if switchOn {
+            return 1
+        } else {
+            return 0
+        }
+    }
     private func animate() {
         if switchOn {
-            let turn_off = SKAction.animateWithTextures([], timePerFrame: 0.5)
+            let textures:[SKTexture] = []
+            let turn_off = SKAction.animateWithTextures(textures, timePerFrame: 0.5)
             self.runAction(turn_off)
             self.switchOn = false
         } else {
-            let turn_on = SKAction.animateWithTextures([], timePerFrame: 0.5)
+            let textures:[SKTexture] = [SKTexture(imageNamed:"redTile2"),SKTexture(imageNamed:"redTile3"),SKTexture(imageNamed:"redTile4"),
+                                        SKTexture(imageNamed:"redTile5"),SKTexture(imageNamed:"blueOne")]
+            let turn_on = SKAction.animateWithTextures(textures, timePerFrame: 0.01)
             self.runAction(turn_on)
             self.switchOn = true
         }
