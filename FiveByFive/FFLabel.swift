02ecab8755d4b04893b29bce6e1dc5ae4662f323
Nick <nick@cracchiolo.com>
2016-05-20 11:13:58 -0700

    Added String and Character Extensions for FFLabel line wrapping

---
+++ FiveByFive/FFLabel.swift
@@ -0,0 +1,114 @@
+//
+//  FFLabel.swift
+//  FiveByFive
+//
+//  Created by Nicholas Cracchiolo on 5/17/16.
+//  Copyright © 2016 Nick Cracchiolo. All rights reserved.
+//
+
+import SpriteKit
+
+class FFLabel: SKNode {
+    var bounds:CGSize
+    var newLineOn:Bool
+    var text:String = "" {
+        didSet {
+            wrapText()
+            //drawBounds()
+        }
+    }
+    var fontSize:CGFloat = 30
+    var fontName:String = Constants.FontName.Game_Font
+    var fontColor:UIColor = UIColor.blackColor()
+    var horizontalAlignmentMode:SKLabelHorizontalAlignmentMode = .Left
+    var verticalAlignmentMode:SKLabelVerticalAlignmentMode = .Baseline
+    
+    init(bounds:CGSize) {
+        self.bounds = bounds
+        self.newLineOn = false
+        super.init()
+        drawBounds()
+    }
+    
+    init(text:String,bounds:CGSize) {
+        self.bounds = bounds
+        self.newLineOn = false
+        self.text = text
+        super.init()
+        drawBounds()
+        
+    }
+    init(text:String, bounds:CGSize, useNewLine:Bool) {
+        self.bounds = bounds
+        self.newLineOn = useNewLine
+        self.text = text
+        super.init()
+        drawBounds()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func wrapText() {
+        self.removeAllChildren()
+        let frameWidth = self.bounds.width
+        let label = SKLabelNode(text: self.text)
+        let labelWidth = label.frame.size.width
+        var str = ""
+        var initalPosition = CGPointMake(0,0)
+        if (labelWidth > frameWidth) {
+            for word in text.words() {
+                let tempString = str + word + " "
+                if checkLength(tempString) {
+                    createSubLabel(str, belowHeight: initalPosition.y)
+                    initalPosition.y = initalPosition.y - label.frame.size.height
+                    str = word
+                } else {
+                    str = tempString
+                }
+            }
+        } else {
+            createSingleLineLabel(initalPosition)
+        }
+    }
+    //Returns true if length is >= the bounds set in init
+    private func checkLength(str:String) -> Bool {
+        let tempLabel = SKLabelNode(text: str)
+        if tempLabel.frame.size.width >= self.bounds.width {
+            return true
+        }
+        return false
+    }
+    private func createSingleLineLabel(atPosition:CGPoint) {
+        let label = SKLabelNode(text: self.text)
+        label.position = atPosition
+        label.fontName = self.fontName
+        label.fontSize = self.fontSize
+        label.fontColor = self.fontColor
+        label.zPosition = 1
+        label.horizontalAlignmentMode = self.horizontalAlignmentMode
+        //label.verticalAlignmentMode = self.verticalAlignmentMode
+        self.addChild(label)
+    }
+    private func createSubLabel(text:String, belowHeight:CGFloat) {
+        let subLabel = SKLabelNode(text: text)
+        subLabel.position = CGPointMake(0/*-CGRectGetMidX(self.frame)*/, belowHeight - subLabel.frame.size.height)
+        subLabel.fontName = self.fontName
+        subLabel.fontSize = self.fontSize
+        subLabel.fontColor = self.fontColor
+        subLabel.zPosition = 1
+        subLabel.horizontalAlignmentMode = self.horizontalAlignmentMode
+        //subLabel.verticalAlignmentMode = self.verticalAlignmentMode
+        self.addChild(subLabel)
+    }
+    //Debugging method to check positioning and node's bounds
+    private func drawBounds() {
+        let rect =  SKShapeNode(rectOfSize: self.bounds)
+        rect.position = CGPointMake(0, 0)
+        rect.lineWidth = 2
+        rect.strokeColor = UIColor.blackColor()
+        rect.fillColor = UIColor.blueColor()
+        rect.zPosition = 0
+        self.addChild(rect)
+        print("Debugging Rect Added")
+    }
+}
---
+++ FiveByFive/TypeExtensions.swift
@@ -0,0 +1,55 @@
+//
+//  TypeExtensions.swift
+//  FiveByFive
+//
+//  Created by Nicholas Cracchiolo on 5/18/16.
+//  Copyright © 2016 Nick Cracchiolo. All rights reserved.
+//
+
+import Swift
+
+extension Character {
+    //Checks if the Character is an operator
+    func isOperator() -> Bool {
+        if self == "+" || self == "-" || self == "/" || self == "*" {
+            return true
+        }
+        return false
+    }
+    //Checks if the Characters is a punctuation mark
+    func isPunctuation() -> Bool {
+        if self == "." || self == "!" || self == "?" {
+            return true
+        }
+        return false
+    }
+    //Checks if the Character is a space
+    func isSpace() -> Bool {
+        if self == " " {
+            return true
+        }
+        return false
+    }
+}
+
+extension String {
+    //Returns an array of all the words in the String
+    func words() -> [String] {
+        var words:[String] = []
+        var word = ""
+        for c in self.characters {
+            if c.isPunctuation() || c.isSpace() {
+                if word != "" {
+                    words.append(word)
+                }
+                word = ""
+            } else {
+                word.append(c)
+            }
+        }
+        if !word.isEmpty {
+            words.append(word)
+        }
+        return words
+    }
+}
